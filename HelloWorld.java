5c6c11babf4c92f89d51fe298f0037e186bee648
JLew15 <zombears@icloud.com>
2020-09-10 10:10:25 -0600

    HEHEHEHEHEHHEHEHEHE

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,19 @@
 class HelloWorld{
 public static void main(String[] args){
   System.out.println("This is not gonna work with ASCII because of the fact that it is just being annoying.");
+  System.out.println("I'm gonna make my own ASCII then...");
+  System.out.println(".");
+  System.out.println("..");
+  System.out.println(". .");
+  System.out.println(".  .");
+  System.out.println(".   .");
+  System.out.println(".    .");
+  System.out.println(".     .");
+  System.out.println(".    .");
+  System.out.println(".   .");
+  System.out.println(".  .");
+  System.out.println(". .");
+  System.out.println("..");
+  System.out.println(".");
 }
 }
