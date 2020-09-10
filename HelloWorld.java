32586b1a13ca89473768e0369677499feb2f5a78
JLew15 <zombears@icloud.com>
2020-09-10 09:33:28 -0600

    Edited Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
 public static void main(String[] args){
-  System.out.println("Hello World!");
+  System.out.println("This is not gonna work with ASCII because of the fact that it is just being annoying.");
 }
 }
