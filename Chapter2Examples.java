f78b509f4f8086ab625cac78b406b2dfe0a95afc
JLew15 <zombears@icloud.com>
2020-09-14 09:37:26 -0600

    fgjhg

---
+++ Chapter2Examples.java
@@ -0,0 +1,55 @@
+/*
+Java Programming
+Chapter 2 Lesson 2
+Examples
+Jaiden Lewis
+09/14/2020
+*/
+
+
+
+
+// Code Statements
+/*
+A piece of code that performs a specific action and ends with a semicolon
+radius = 1;
+
+radius = 1; diameter = 2 * radius;
+radius=1;
+diameter=2*radius;
+*/
+
+class Chapter2Examples{
+  //Main entry point to the program
+
+  public static void main(String[] args){
+    String name = "Jay";
+    int age = 17;
+    String birthdate = "XX/XX/20XX";
+    double gpa = 3.42;
+    char gender = 'M';
+    System.out.println("Hello, World");
+    System.out.println("My name is "+ name);
+    System.out.println("I am "+ age +" years old.");
+    System.out.println("I was born on "+ birthdate);
+    System.out.println("I have an average GPA of "+ gpa);
+
+  }
+}
+
+/*
+.jpg image file
+.mp3 audio file
+.img
+.wav
+.exe
+.svg
+.ico
+.gif
+.py
+.cpp
+.cs
+.bash
+.java SourceFile
+.class compiled file
+*/
